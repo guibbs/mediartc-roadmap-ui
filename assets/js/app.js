37e1d832e532ed345465040c65927eabf0868505
Guillaume Baudusseau <guillaume.baudusseau@gmail.com>
2016-05-02 16:33:53 -0400

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+(function() {
+
+    var menuLeftBtn = document.getElementById('menu-left-btn');
+
+    menuLeftBtn.addEventListener('click', function() {
+
+        leftPane.className += ' active';
+
+    }, true);
+
+})();
