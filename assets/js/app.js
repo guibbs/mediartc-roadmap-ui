f5af4d5e379ebaf64ecec395a8307e9fbf8e2a10
Guillaume Baudusseau <guillaume.baudusseau@gmail.com>
2016-05-03 16:24:48 -0400

    remove useless script

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,4 @@
 (function() {
 
-    var menuLeftBtn = document.getElementById('menu-left-btn');
-
-    menuLeftBtn.addEventListener('click', function() {
-
-        leftPane.className += ' active';
-
-    }, true);
 
 })();
