37156dd974f37a25c0804aaf3384daca237ad36e
Guillaume Baudusseau <guillaume.baudusseau@gmail.com>
2016-06-15 13:58:06 -0400

    add pages for all categories

--- assets/js/side-nav.js
+++ assets/js/side-nav.js
@@ -1,22 +1,19 @@
 (function () {
+  var sideNavBtn = document.getElementsByName('sidenav-opener')[0]
+  var sideNavOverlay = document.getElementById('side-nav-overlay')
+  var sideNavContent = document.getElementById('side-nav-content')
 
-    var sideNav = document.getElementById('side-nav');
-    var sideNavBtn = document.getElementById('side-nav-btn');
-    var sideNavOverlay = document.getElementById('side-nav-overlay');
-    var sideNavContent = document.getElementById('side-nav-content');
+  sideNavBtn.addEventListener('click', function () {
+    sideNavOverlay.className += ' active'
+    sideNavContent.className += ' active'
+  })
 
-    sideNavBtn.addEventListener('click', function() {
-        sideNavOverlay.className += ' active';
-        sideNavContent.className += ' active';
-    });
+  sideNavOverlay.addEventListener('click', function () {
+    sideNavOverlay.className = sideNavOverlay.className.replace('active', 'hidden')
+    sideNavContent.className = sideNavContent.className.replace('active', 'hidden')
+  })
 
-    sideNavOverlay.addEventListener('click', function() {
-        sideNavOverlay.className = sideNavOverlay.className.replace('active', 'hidden');
-        sideNavContent.className = sideNavContent.className.replace('active', 'hidden');
-    });
+  sideNavContent.addEventListener('click', function () {
 
-    sideNavContent.addEventListener('click', function() {
-
-    });
-
+  })
 })()
